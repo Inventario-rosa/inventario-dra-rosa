e77ba803e89146bbe3bb899c57005cd38d847ff8
Inventario-rosa <patoboterom@gmail.com>
2025-06-23 11:52:05 -0500

    Delete InventarioMedicamentos.jsx

--- InventarioMedicamentos.jsx
+++
@@ -1,5 +0,0 @@
-// Aquí irá el contenido React completo que has validado anteriormente.
-// Lo simplificamos por ahora como referencia.
-export default function InventarioMedicamentos() {
-  return <div>App de inventario lista para implementar funcionalidades.</div>;
-}
