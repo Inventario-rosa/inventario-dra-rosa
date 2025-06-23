9c403914c6315fc8c21167d6d8c502c1567632bc
Inventario-rosa <patoboterom@gmail.com>
2025-06-23 11:51:44 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import InventarioMedicamentos from './InventarioMedicamentos'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <InventarioMedicamentos />
-  </React.StrictMode>
-)
