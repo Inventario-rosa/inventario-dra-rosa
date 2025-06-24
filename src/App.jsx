118c809b5b1a03d8e536ff4fec7660d242d0a7b1
Inventario-rosa <patoboterom@gmail.com>
2025-06-23 20:26:09 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import IngresoMedicamento from './IngresoMedicamento';
+import GestionUsuarios from './GestionUsuarios';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Sistema Inventario Dra. Rosa</h1>
+      <IngresoMedicamento />
+      <hr />
+      <GestionUsuarios />
+    </div>
+  );
+}
